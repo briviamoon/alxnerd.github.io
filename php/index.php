c5966ed873240ef37355a0730fba831284ac6d52
briviamoon <briviamoon@gmail.com>
2023-11-04 18:24:39 +0300

    add atricle page template

---
+++ php/index.php
@@ -0,0 +1,33 @@
+<?php include('../php/components.php'); ?>
+
+<?php echo $pageHead; ?>
+
+<body>
+	<!-- nav-bar-header -->
+	<?php echo $pageNavBar; ?>
+
+	<section class="post-section section" id="postsection">
+		<div class="article-space">
+			<div class="article-title place-items-center">
+				<h1 class="title">ARTICLE</h1>
+				<p>Lorem ipsum dolor sit amet consectetur?</p>
+			</div>
+
+			<div class="image-box">
+				<img src="../img/alx-c.png" alt="" class="article-img">
+			</div>
+
+			<div class="article-box container">
+				<!-- Article goes here -->
+				<div class="article-content">
+					<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Suscipit doloremque accusantium consequuntur, nulla illo rerum assumenda laborum maxime quas quos delectus sed harum excepturi ea? Eos fugiat ipsam ipsa in. Lorem ipsum dolor sit amet consectetur adipisicing elit. A possimus nisi dicta fugiat vel. Deserunt nemo aut voluptatibus rerum dolorum inventore quod tempora maiores. Architecto magnam labore fugit dolorem cum!. Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nemo eveniet soluta repellat. Vitae ratione vero cum vel quos debitis, quod pariatur maxime praesentium ipsam ipsa provident itaque ex eveniet id. Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt quisquam facilis expedita repudiandae labore facere accusamus tenetur, commodi ad consequatur quae! Qui, aspernatur aliquid eius eveniet accusantium quas quibusdam praesentium. Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore similique quis non voluptas iste, odit porro fuga repellendus, rem pariatur delectus excepturi corrupti minus beatae quo dolor corporis consequuntur tenetur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, quo! Repudiandae vitae itaque enim, in dolor, culpa nulla suscipit voluptatibus laboriosam iste cum dolores illo? Inventore voluptates explicabo debitis voluptatum!.Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat, porro labore! Dolorem quia illo cumque, voluptatum, corrupti, eum nostrum odit doloremque vitae enim placeat. Doloremque saepe impedit culpa magni maiores!</p>
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- js -->
+	<?php echo $headerJs; ?>
+</body>
+
+</html>
